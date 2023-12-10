c6a2c30a3490037da9edf74c6fee10b60d8c596c
鈴木 映美 <yon.ncity@gmail.com>
2023-12-10 20:53:27 +0900

    都道府県の辞書を作成

--- WinFormsApp7_3/Form1.cs
+++ WinFormsApp7_3/Form1.cs
@@ -2,9 +2,23 @@ namespace WinFormsApp7_3
 {
     public partial class Form1 : Form
     {
+
+        //
+        Dictionary<string, string> cityNames;
+
         public Form1()
         {
             InitializeComponent();
+
+
+            // “s“¹•{Œ§‚Ì«‘‚ğì¬‚·‚é
+            this.cityNames = new Dictionary<string, string>();
+
+            this.cityNames.Add("Ã‰ªŒ§","‚P");
+            this.cityNames.Add("“Œ‹“s","‚Q");
+            this.cityNames.Add("ç—tŒ§","‚R");
+            this.cityNames.Add("L“‡Œ§","‚S");
+
         }
 
         private void CitySelected(object sender, EventArgs e)
